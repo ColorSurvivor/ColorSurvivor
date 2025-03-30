86e24b3df72aba306c94e427240efbafc7f539f5
JoSungbin <sdgundam56@gmail.com>
2025-03-30 17:20:12 +0900

    FirstSimpleBase
    심플하게 엔티티 베이스 잡아봤습니다.

---
+++ SalaryHunter20XX/Assets/Scripts/Bullet.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{   
+    Vector2 GoDir; //탄막의 진행방향
+    int DMG = 5; //탄막의 데미지
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
