3373622138f7d740f893d6b2f36dffa4f2b569fc
JoSungbin <sdgundam56@gmail.com>
2025-05-15 19:44:22 +0900

    스킬 베이스2
    기존에 UI관련이랑 연동

---
+++ SalaryHunter20XX/Assets/Scripts/ExampleBullet.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using UnityEngine;
+
+public class ExampleBullet : MonoBehaviour
+{
+    public int DMG = 5; //탄막의 데미지
+    int SPD = 10; //탄막의 속도
+    Rigidbody2D RD;
+    public Vector2 GoVec; //날아갈 방향
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        RD = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    public void Shot() //발사
+    {
+        RD = GetComponent<Rigidbody2D>();
+        RD.linearVelocity = GoVec.normalized*SPD; //날아갈 방향 * 속도
+        StartCoroutine(Timer()); //자괴
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+{
+    if (collision.CompareTag("Enemy"))
+    {
+        var enemy = collision.GetComponent<Monster>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(DMG); // ← 맞은 적이 스스로 처리함
+        }
+
+        Destroy(gameObject);
+    }
+}
+    IEnumerator Timer() //너무 멀리 날아가면 자괴
+    {
+        yield return new WaitForSeconds(3f);
+        Destroy(gameObject);
+    }
+}
---
+++ SalaryHunter20XX/Assets/Scripts/Reference.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public enum ColorType
+{
+    Red,
+    Green,
+    Blue
+}
+
+public enum WeaponGrade
+{
+    Common,
+    Rare,
+    Epic,
+    Legendary
+}
+
+public enum WeaponType
+{
+    Bow,
+    CrossBow,
+    Dagger,
+    Staff
+}
+
+public static class GameConstants
+{
+    public const float SameColorBonusMultiplier = 1.3f;
+    public const float DifferentColorPenaltyMultiplier = 0.8f;
+    public const int MaxWeaponSlots = 3;
+    public const int MaxPassiveSlots = 5;
+}
