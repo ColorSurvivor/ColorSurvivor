68c85238ab13f4747dd8cb6fd7fbaf6384ec76a0
dpwns5889 <dpwns5889@naver.com>
2025-05-23 01:26:41 +0900

    fixed
    fix

--- SalaryHunter20XX/Assets/Scripts/Player/Player.cs
+++ SalaryHunter20XX/Assets/Scripts/Player/Player.cs
@@ -153,9 +153,11 @@ public class Player : Entity
             Time.timeScale = 0;
         }
     }
-   public void GetWeapon(GameObject newWeapon)
+   public void GetWeapon(WeaponData newWeapon)
     {
-        GameObject temp = Instantiate(newWeapon);
+        GameObject temp = Instantiate(newWeapon.weaponPrefab);
+        temp.GetComponent<BaseGun>().Init(newWeapon);
+        GameManager.instance.itemPool.curWeapons.Add(temp);
         temp.transform.SetParent(GameManager.instance.player.GetComponent<Player>().weaponHanger.transform);
         temp.transform.position = transform.position;
     }
