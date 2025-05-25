9c26672305937e726591da2659f12da70657ec53
ssm3842 <ssm3842@naver.com>
2025-05-25 23:05:28 +0900

    해상도 옵션 제거
    스케일러 수정 실패

--- SalaryHunter20XX/Assets/Scripts/UI/OptionMenu.cs
+++ SalaryHunter20XX/Assets/Scripts/UI/OptionMenu.cs
@@ -1,22 +1,14 @@
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
-using System.Collections.Generic;
 
 public class OptionMenu : MonoBehaviour
 {
     [Header("UI References")]
     public Slider volumeSlider;
     public Toggle fullscreenToggle;
-    public TMP_Dropdown resolutionDropdown;
 
-    private Resolution[] allowedResolutions = new Resolution[]
-    {
-        new Resolution { width = 1280, height = 720 },
-        new Resolution { width = 1920, height = 1080 },
-        new Resolution { width = 2560, height = 1440 },
-        new Resolution { width = 3840, height = 2160 }
-    };
+    private int targetWidth = 1920;
+    private int targetHeight = 1080;
 
     void Start()
     {
@@ -28,28 +20,8 @@ public class OptionMenu : MonoBehaviour
         fullscreenToggle.isOn = Screen.fullScreen;
         fullscreenToggle.onValueChanged.AddListener(SetFullscreen);
 
-        // 해상도 드롭다운 설정
-        resolutionDropdown.ClearOptions();
-
-        List<string> options = new List<string>();
-        int currentResolutionIndex = 0;
-
-        for (int i = 0; i < allowedResolutions.Length; i++)
-        {
-            string option = allowedResolutions[i].width + " x " + allowedResolutions[i].height;
-            options.Add(option);
-
-            if (Screen.currentResolution.width == allowedResolutions[i].width &&
-                Screen.currentResolution.height == allowedResolutions[i].height)
-            {
-                currentResolutionIndex = i;
-            }
-        }
-
-        resolutionDropdown.AddOptions(options);
-        resolutionDropdown.value = currentResolutionIndex;
-        resolutionDropdown.RefreshShownValue();
-        resolutionDropdown.onValueChanged.AddListener(SetResolution);
+        // 무조건 FHD로 고정
+        Screen.SetResolution(targetWidth, targetHeight, Screen.fullScreen);
     }
 
     public void SetVolume(float volume)
@@ -60,16 +32,13 @@ public class OptionMenu : MonoBehaviour
     public void SetFullscreen(bool isFullscreen)
     {
         Screen.fullScreen = isFullscreen;
-    }
 
-    public void SetResolution(int index)
-    {
-        Resolution res = allowedResolutions[index];
-        Screen.SetResolution(res.width, res.height, Screen.fullScreen);
+        // 전체화면 상태 바꿨을 때도 해상도 유지
+        Screen.SetResolution(targetWidth, targetHeight, isFullscreen);
     }
 
     public void CloseOptionPanel()
     {
-        gameObject.SetActive(false); // 패널 비활성화
+        gameObject.SetActive(false);
     }
 }
