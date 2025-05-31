ae5e58aa78c9f9e42ef89f8616e2f5b517b5971c
ssm3842 <ssm3842@naver.com>
2025-05-31 21:08:27 +0900

    옵션창 수정

--- SalaryHunter20XX/Assets/Scripts/UI/OptionMenu.cs
+++ SalaryHunter20XX/Assets/Scripts/UI/OptionMenu.cs
@@ -41,4 +41,9 @@ public class OptionMenu : MonoBehaviour
     {
         gameObject.SetActive(false);
     }
+
+    public void TogglePanel(GameObject panel)
+    {
+        panel.SetActive(!panel.activeSelf);
+    }
 }
